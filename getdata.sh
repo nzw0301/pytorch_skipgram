6a7a859624c1acb1c8fb7eaefb07f289046dbf1b
Kento Nozawa <k_nzw@klis.tsukuba.ac.jp>
2019-03-01 23:34:08 +0900

    Update getdata.sh

--- getdata.sh
+++ getdata.sh
@@ -20,6 +20,7 @@ cd ..
 
 echo "- Downloading text8"
 if [ ! -e text8 ]; then
-    wget http://mattmahoney.net/dc/text8.zip -O text8.gz
-    gzip -d text8.gz -f # for linux
+    wget http://mattmahoney.net/dc/text8.zip
+    unzip text8.zip
+    rm text8.zip
 fi
